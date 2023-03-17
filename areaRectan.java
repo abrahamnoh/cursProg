ea303ea7200723ba631eb598732519b7b82008f6
abrahamnoh <abrahamnoh07@gmail.com>
2023-03-17 14:01:41 -0600

    areaRectan

---
+++ areaRectan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class areaRectan {
+    public static void main(String[] args){
+
+        int a;
+        int b;
+        int c;
+        System.out.println("Area de un rectangulo");
+        System.out.println("Supongamos que la altura del rectangulo es 4");
+        System.out.println("Supongamos que la base del rectangulo es 8");
+        a=4;
+        b=8;
+        c=a*b;
+        System.out.println("El area del rectangulo es: "+c);
+    }
+    
+}
